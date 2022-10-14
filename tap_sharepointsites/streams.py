46c95d04568ddaa000e9d1cd668905ba70a22827
Henning <henning.holgersen@gmail.com>
2022-10-14 22:04:02 +0200

    init

---
+++ tap_sharepointsites/streams.py
@@ -0,0 +1,29 @@
+"""Stream type classes for tap-sharepointsites."""
+
+from pathlib import Path
+from typing import Any, Dict, Optional, Union, List, Iterable
+
+from singer_sdk import typing as th  # JSON Schema typing helpers
+
+from tap_sharepointsites.client import sharepointsitesStream
+
+# TODO: Delete this is if not using json files for schema definition
+SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
+# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
+#       - Copy-paste as many times as needed to create multiple stream types.
+
+
+class ListStream(sharepointsitesStream):
+    """Define custom stream."""
+    name = "lists"
+    path = ""
+    primary_keys = [] #["@odata.etag"]
+    replication_key = None
+    schema = th.PropertiesList(
+        th.Property("@odata.etag", th.StringType),
+        th.Property("createdDateTime", th.StringType),
+        th.Property("id", th.StringType),
+        th.Property("webUrl", th.StringType),
+        th.Property("fields@odata.context", th.StringType),
+        th.Property("fields", th.StringType),
+    ).to_dict()
