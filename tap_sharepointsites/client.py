e8895e9ac5db2376e657b71ec556bd6b7c586a89
Henning <henning.holgersen@gmail.com>
2022-10-15 10:41:12 +0200

    tidying

--- tap_sharepointsites/client.py
+++ tap_sharepointsites/client.py
@@ -27,8 +27,6 @@ class sharepointsitesStream(RESTStream):
     """sharepointsites stream class."""
 
     # TODO: Set the API's base URL here:
-    # url_base = "https://graph.microsoft.com/v1.0/"
-    # https://graph.microsoft.com/{version}/{resource}?{query-parameters}
 
     # OR use a dynamic url_base:
     @property
@@ -69,36 +67,6 @@ class sharepointsitesStream(RESTStream):
 
     def get_new_paginator(self):
         return GraphHATEOASPaginator()
-
-    # def get_next_page_token(
-    #     self, response: requests.Response, previous_token: Optional[Any]
-    # ) -> Optional[Any]:
-    #     """Return a token for identifying next page or None if no more pages."""
-    #     # TODO: If pagination is required, return a token which can be used to get the
-    #     #       next page. If this is the final page, return "None" to end the
-    #     #       pagination loop.
-
-    #     # nextpage = extract_jsonpath(self.next_page_token_jsonpath, input=response.json())
-    #     nextpage = response.json().get("@odata.nextLink")
-    #     if nextpage:
-    #         base_token = re.findall(r"\$skiptoken=(.*)", nextpage)[-1]
-    #         return {"$skiptoken": base_token}
-    #         # return f"$skiptoken={base_token}"
-    #     else:
-    #         return None
-
-
-    # def get_url_params(
-    #     self, context: Optional[dict], next_page_token: Optional[Any]
-    # ) -> Dict[str, Any]:
-    #     """Return a dictionary of values to be used in URL parameterization."""
-    #     params: dict = {}
-    #     if next_page_token:
-    #         params["page"] = next_page_token
-    #     if self.replication_key:
-    #         params["sort"] = "asc"
-    #         params["order_by"] = self.replication_key
-    #     return params
 
 
     def get_url_params(
