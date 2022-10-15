7a8d5027843a57f24eadfd9082d332d1482f26d0
Henning <henning.holgersen@gmail.com>
2022-10-15 12:26:00 +0200

    complete attr list + debug

--- tap_sharepointsites/streams.py
+++ tap_sharepointsites/streams.py
@@ -17,13 +17,19 @@ class ListStream(sharepointsitesStream):
     """Define custom stream."""
     name = "lists"
     path = ""
-    primary_keys = ["'@odata.etag'"]
+    primary_keys = ["id"]
     replication_key = None
     schema = th.PropertiesList(
         th.Property("@odata.etag", th.StringType),
         th.Property("createdDateTime", th.StringType),
+        th.Property("eTag", th.StringType),
         th.Property("id", th.StringType),
+        th.Property("lastModifiedDateTime", th.StringType),
         th.Property("webUrl", th.StringType),
+        th.Property("createdBy", th.StringType),
+        th.Property("lastModifiedBy", th.StringType),
+        th.Property("parentReference", th.StringType),      
+        th.Property("contentType", th.StringType),
         th.Property("fields@odata.context", th.StringType),
         th.Property("fields", th.StringType),
     ).to_dict()
