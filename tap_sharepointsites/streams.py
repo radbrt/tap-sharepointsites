606b6ba1e64211fb127a1439039e0ac6fce3d7c9
Henning <henning.holgersen@gmail.com>
2022-10-15 13:32:46 +0200

    added objectType for arbitrary json

--- tap_sharepointsites/streams.py
+++ tap_sharepointsites/streams.py
@@ -4,13 +4,16 @@ from pathlib import Path
 from typing import Any, Dict, Optional, Union, List, Iterable
 
 from singer_sdk import typing as th  # JSON Schema typing helpers
-
+from singer_sdk.typing import (
+    PropertiesList,
+    Property,
+    StringType,
+    ObjectType
+)
 from tap_sharepointsites.client import sharepointsitesStream
 
 # TODO: Delete this is if not using json files for schema definition
 SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
 
 
 class ListStream(sharepointsitesStream):
@@ -19,17 +22,17 @@ class ListStream(sharepointsitesStream):
     path = ""
     primary_keys = ["id"]
     replication_key = None
-    schema = th.PropertiesList(
-        th.Property("@odata.etag", th.StringType),
-        th.Property("createdDateTime", th.StringType),
-        th.Property("eTag", th.StringType),
-        th.Property("id", th.StringType),
-        th.Property("lastModifiedDateTime", th.StringType),
-        th.Property("webUrl", th.StringType),
-        # th.Property("createdBy", th.StringType),
-        # th.Property("lastModifiedBy", th.StringType),
-        # th.Property("parentReference", th.StringType),      
-        # th.Property("contentType", th.StringType),
-        # th.Property("fields@odata.context", th.StringType),
-        # th.Property("fields", th.StringType),
+    schema = PropertiesList(
+        Property("@odata.etag", StringType),
+        Property("createdDateTime", StringType),
+        Property("eTag", StringType),
+        Property("id", StringType),
+        Property("lastModifiedDateTime", StringType),
+        Property("webUrl", StringType),
+        Property("createdBy", ObjectType()),
+        Property("lastModifiedBy", ObjectType()),
+        Property("parentReference", ObjectType()),      
+        Property("contentType", ObjectType()),
+        Property("fields@odata.context", ObjectType()),
+        Property("fields",  ObjectType()),
     ).to_dict()
